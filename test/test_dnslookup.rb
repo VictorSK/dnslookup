77976ebd703f02e66f218cee37b30a1e8597dfc6
Victor S. Keenan <victor@inspyre.com>
2016-03-04 19:03:30 -0500

    Add complete test coverage.

--- test/test_dnslookup.rb
+++ test/test_dnslookup.rb
@@ -2,20 +2,52 @@ require 'minitest/autorun'
 require 'dnslookup'
 
 class DNSLookupTest < Minitest::Test
+
   def setup
-    ARGV[0] = 'victorkeenan.com'
-    out, err = capture_io do
+    ARGV[0] = 'google.com'
+  end
+
+  def call_class
+    @out, @err = capture_io do
       @dnslookup = DNSLookup.new
     end
+
+  end
+
+  def test_mx_lookup
+    ARGV[1] = '-m'
+    call_class
+    assert_includes @out, '10 aspmx.l.google.com.'
+  end
+
+  def test_a_name_lookup
+    ARGV[1] = '-a'
+    call_class
+    assert_includes @out, '65.196.188.57'
+  end
+
+  def test_t_name_lookup
+    ARGV[1] = '-t'
+    call_class
+    assert_includes @out, "v=spf1 include:_spf.google.com ~all"
+  end
+
+  def test_c_name_lookup
+    ARGV[0] = 'mail.google.com'
+    ARGV[1] = '-c'
+    call_class
+    assert_includes @out, 'googlemail.l.google.com.'
   end
 
   def test_setup_query_servers_single
-    @single_server = '1.2.3.4'
-    assert @dnslookup.setup_query_servers, '1.2.3.4'
+    ARGV[1] = '-s8.8.4.4'
+    call_class
+    assert_equal ['8.8.4.4'], @dnslookup.setup_query_servers
   end
 
   def test_setup_query_servers
     test_servers = ['8.8.8.8', '8.8.4.4']
-    assert @dnslookup.setup_query_servers, ['8.8.8.8', '8.8.4.4']
+    call_class
+    assert_equal test_servers, @dnslookup.setup_query_servers
   end
 end
